eaaaa66918429f00088a8a8c2545e60490f0aca6
javiluli <javiluli.dev@gmail.com>
2022-02-02 20:11:53 +0100

    feat: se ha cambiado la conexion a la BD por un pool de conexiones

--- src/daos/TransaccionesManager.java
+++ src/daos/TransaccionesManager.java
@@ -4,7 +4,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Savepoint;
 
-import bbdd.ConexionOracle;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import exceptions.DAOException;
 
 public class TransaccionesManager {
@@ -15,27 +19,33 @@ public class TransaccionesManager {
   private static final String DB_CON_ERR = "Error al conectar con la base de datos";
 
   // para obtener la conexion del pool de conexiones, ver WEB.xml
-  /*
-   * public TransaccionesManager() throws DAOException { try { Context ctx = new
-   * InitialContext(); DataSource ds = (DataSource)
-   * ctx.lookup("java:comp/env/PoolDeConexiones"); con = ds.getConnection();
-   * con.setAutoCommit(false); } catch (SQLException e) { throw new
-   * DAOException(DB_ERR, e); } catch (NamingException e) { throw new
-   * DAOException(DB_CON_ERR, e); } }
-   */
-  // la conexion la establezco con la clase ConexionOracle
+
   public TransaccionesManager() throws DAOException {
-
-    // con = new ConexionOracle("jdbc:oracle:thin:@alfon-bb574d6f5:1521:oracle",
-    // "adrian", "adrian").getConexion();
-    con = new ConexionOracle().getConexion();
-
     try {
+      Context ctx = new InitialContext();
+      DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/PoolDeConexionesOracle");
+      con = ds.getConnection();
       con.setAutoCommit(false);
     } catch (SQLException e) {
       throw new DAOException(DB_ERR, e);
+    } catch (NamingException e) {
+      throw new DAOException(DB_CON_ERR, e);
     }
   }
+
+  // la conexion la establezco con la clase ConexionOracle
+//  public TransaccionesManager() throws DAOException {
+//
+//    // con = new ConexionOracle("jdbc:oracle:thin:@alfon-bb574d6f5:1521:oracle",
+//    // "adrian", "adrian").getConexion();
+//    con = new ConexionOracle().getConexion();
+//
+//    try {
+//      con.setAutoCommit(false);
+//    } catch (SQLException e) {
+//      throw new DAOException(DB_ERR, e);
+//    }
+//  }
 
   public void closeCommit() throws DAOException {
     try {
